7f120cf49c312371131178ac03d36c91d54c726a
Rita Gonzalez Hesaynes <rita@magoya.io>
2018-11-22 20:19:51 -0300

    Fix things

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,7 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post-detail.html', {'post': post})
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
     if request.method == 'POST':
@@ -22,4 +22,17 @@ def post_new(request):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request, 'blog/post-edit.html', {'form': form})
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
